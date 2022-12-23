274b56983ec53a68bcc7b96c31c7986b1a21438b
Peter Schmidt <schmidt.multimedia@gmail.com>
2022-12-23 09:24:55 +0000

    Test der Block-Funktionalität

--- dev-test.js
+++ dev-test.js
@@ -1,1 +1,9 @@
-console.log("Pause");
+// call: npm run dev-test
+
+// Import
+const Block = require("./block");
+
+/***  Test der Block-Funktionalität  */
+const block = new Block("aktuelle Zeit","hash1","hash2","meineDaten");
+console.log(block.toString());
+
